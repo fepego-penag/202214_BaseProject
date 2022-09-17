f0bac8a3f9210864a4b971eb9350f89fc50ecbc2
fepego-penag <cf.pena1138@uniandes.edu.co>
2022-09-17 11:50:13 -0500

    adding club service

--- src/club/club.service.ts
+++ src/club/club.service.ts
@@ -1,4 +1,76 @@
 import { Injectable } from '@nestjs/common';
+import { ClubEntity } from './club.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import {
+  BusinessLogicException,
+  BusinessError,
+} from '../shared/errors/business-errors';
 
 @Injectable()
-export class ClubService {}
+export class ClubService {
+  constructor(
+    @InjectRepository(ClubEntity)
+    private readonly clubRepository: Repository<ClubEntity>,
+  ) {}
+
+  async findAll(): Promise<ClubEntity[]> {
+    return await this.clubRepository.find({
+      relations: ['members'],
+    });
+  }
+
+  async findOne(id: string): Promise<ClubEntity> {
+    const club: ClubEntity = await this.clubRepository.findOne({
+      where: { id },
+      relations: ['members'],
+    });
+    if (!club)
+      throw new BusinessLogicException(
+        'Club not found',
+        BusinessError.NOT_FOUND,
+      );
+
+    return club;
+  }
+
+  async create(club: ClubEntity): Promise<ClubEntity> {
+    return await this.clubRepository.save(club);
+  }
+
+  async update(id: string, club: ClubEntity): Promise<ClubEntity> {
+    const clubDb: ClubEntity = await this.clubRepository.findOne({
+      where: { id },
+    });
+    if (!clubDb)
+      throw new BusinessLogicException(
+        'Club not found',
+        BusinessError.NOT_FOUND,
+      );
+
+    return await this.clubRepository.save({ ...clubDb, ...club });
+  }
+
+  async delete(id: string) {
+    const club: ClubEntity = await this.clubRepository.findOne({
+      where: { id },
+    });
+    if (!club)
+      throw new BusinessLogicException(
+        'Club not found',
+        BusinessError.NOT_FOUND,
+      );
+
+    await this.clubRepository.remove(club);
+  }
+
+  private emailValidation(email: string): boolean {
+    let isvalid = false;
+    const regexp = new RegExp(
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    );
+
+    isvalid = regexp.test(email);
+    return isvalid;
+  }
+}
